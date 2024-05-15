1acbe61b9f6aa73b8e77118b19c4cb1bbf9f997e
munees <munees@techbank.finance>
2024-05-15 11:23:30 +0530

    fixes

--- src/components/technologies/blockchain/BlockchainServices.vue
+++ src/components/technologies/blockchain/BlockchainServices.vue
@@ -3,9 +3,11 @@
     <img class="absolute -z-10" src="../../../assets/img/tech-svg.svg" alt="">
     <div class="lg:px-12 py-10 px-2 lg:py-20 sm:py-38 order-1 lg:order-2 w-full flex justify-center">
       <div class="text-center  flex  flex-col justify-center items-center w-full lg:text-center font-sans space-y-12">
-        <h1 class="text-3xl roundedvector relative  md:text-4xl lg:text-5xl xl:text-6xl sans 2xl:text-6xl font-bold tracking-tight text-white">
+        <div class="relative">
+          <h1 class="text-3xl roundedvector relative  md:text-4xl lg:text-5xl xl:text-6xl sans 2xl:text-6xl font-bold tracking-tight text-white">
           Our Services
         </h1>
+        </div>
         <div class="flex flex-wrap mx-auto justify-center transition-all duration-1000 gap-5 w-full">
           <div data-aos="fade-up" data-aos-offset="200" data-aos-duration="1000" data-aos-easing="ease-in-out"
             class="p-[1px] md:basis-1/4 basis-auto rounded-xl w-full hover:bg-gradient-to-br hover:from-green-500 hover:via-blue-200 hover:to-purple-600 bg-gray-500">
